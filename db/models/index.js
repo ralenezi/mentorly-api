601f174b13218cf3ecb090d7966b2133b8c77db2
Omar <omsi96@users.noreply.github.com>
2020-12-21 19:45:09 +0300

    folder heriarchy

---
+++ db/models/index.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+/** Connect is a method that connects the database to postgres database
+ * @param {function} cb - your call back function such as app.listen or anything you want to happen when the database is connected
+ * @param {object} options - alter: true or force true. By default it's `alter: true`
+ */
+db.connect = async (cb, options = { alter: true }) => {
+  try {
+    await db.sequelize.sync(options);
+    console.log("Connection to the database successful!");
+    cb();
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+  }
+};
+
+module.exports = db;
