63b7815151343b8bf85a36847afa4c00e773cef0
Omar <omsi96@users.noreply.github.com>
2020-12-27 12:17:22 +0300

    Fixing a bug

--- routers/CRUDRouter.js
+++ routers/CRUDRouter.js
@@ -3,10 +3,11 @@ const express = require("express");
 class CRUDRouter extends express.Router {
   constructor(crudController, options) {
     super();
-    this.get("/", options.listMW ?? () => {}, crudController.list);
-    this.post("/create", options.createMW, crudController.create);
-    this.put("/:id", options.updateMW, crudController.update);
-    this.delete("/:id", options.destroyMW, crudController.destory);
+    function emptyFun() {}
+    this.get("/", options?.listMW ?? emptyFun, crudController.list);
+    this.post("/create", options?.createMW ?? emptyFun, crudController.create);
+    this.put("/:id", options?.updateMW ?? emptyFun, crudController.update);
+    this.delete("/:id", options?.destroyMW ?? emptyFun, crudController.destory);
   }
 }
 module.exports = CRUDRouter;
